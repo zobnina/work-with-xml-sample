b7e60aaf1d11127a32a0fe1c96ae116502d30d39
Aleksandra Zobnina <azobnina@neoflex.ru>
2020-12-03 09:28:20 +0300

    init

---
+++ src/main/java/com/example/xmlsdo/XmlSdoApplication.java
@@ -0,0 +1,13 @@
+package com.example.xmlsdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlSdoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlSdoApplication.class, args);
+    }
+
+}
