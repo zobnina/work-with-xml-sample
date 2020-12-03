41e68fb5e022e13e0b1396ca22f80180f34fdd1c
Aleksandra Zobnina <azobnina@neoflex.ru>
2020-12-03 12:14:27 +0300

    xml to json using sdo

--- src/main/java/com/example/xmlsdo/XmlRouteBuilder.java
+++ src/main/java/com/example/xmlsdo/XmlRouteBuilder.java
@@ -1,20 +1,19 @@
 package com.example.xmlsdo;
 
-import commonj.sdo.DataObject;
-import commonj.sdo.helper.XMLDocument;
-import commonj.sdo.helper.XMLHelper;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 @Component
 public class XmlRouteBuilder extends RouteBuilder {
+
     private static final Logger LOGGER = LogManager.getLogger(XmlRouteBuilder.class);
+
+    @Autowired
+    XmlSdoProcessor xmlSdoProcessor;
 
     @Override
     public void configure() throws Exception {
@@ -23,27 +22,11 @@ public class XmlRouteBuilder extends RouteBuilder {
                 .host("{{camel.rest.host}}")
                 .port("{{camel.rest.port}}");
 
-        rest("{{route.add-product-sdo}}")
+        rest("{{route.xml-json-sdo}}")
                 .post()
                 .route()
-                .process(exchange -> {
-                    String xmlString = exchange.getIn().getBody(String.class);
-                    XMLDocument xmlProductDocument = XMLHelper.INSTANCE.load(xmlString);
-                    DataObject productDataObject = xmlProductDocument.getRootObject();
-                    DataObject reviewsFataObject = productDataObject.getDataObject("reviews");
-                    List<DataObject> reviewsDataObjects = reviewsFataObject.getList("review");
-                    Map<String, String> reviews = new LinkedHashMap<>();
-                    for(DataObject reviewDataObject: reviewsDataObjects){
-                        String author = reviewDataObject.getString("author");
-                        String text = reviewDataObject.getString("text");
-                        reviews.put(author,text);
-                    }
-                    Product product = new Product();
-                    product.setName(productDataObject.getString("name"));
-                    product.setPrice(productDataObject.getDouble("price"));
-                    product.setAvailability(productDataObject.getBoolean("availability"));
-                    product.setReviews(reviews);
-                    LOGGER.info(product);
-                });
+                .process(xmlSdoProcessor)
+                .to("{{route.json-out-file}}")
+        .log(LoggingLevel.DEBUG, "${body}");
     }
 }
---
+++ src/main/java/com/example/xmlsdo/XmlSdoProcessor.java
@@ -0,0 +1,56 @@
+package com.example.xmlsdo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import commonj.sdo.DataObject;
+import commonj.sdo.helper.XMLDocument;
+import commonj.sdo.helper.XMLHelper;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class XmlSdoProcessor implements Processor {
+    private static final Logger LOGGER = LogManager.getLogger(XmlSdoProcessor.class);
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        String xmlString = exchange.getIn().getBody(String.class);
+        XMLDocument xmlProductDocument = XMLHelper.INSTANCE.load(xmlString);
+        DataObject productDataObject = xmlProductDocument.getRootObject();
+        Product product = createProduct(productDataObject);
+        ObjectMapper productMapper = new ObjectMapper();
+        String productJson = productMapper.writeValueAsString(product);
+        exchange.getOut().setBody(productJson);
+        //exchange.getIn().setBody(productJson);
+    }
+
+    private Product createProduct(DataObject productDataObject) {
+        Product product = new Product();
+        product.setName(productDataObject.getString("name"));
+        product.setPrice(productDataObject.getDouble("price"));
+        product.setAvailability(productDataObject.getBoolean("availability"));
+        DataObject reviewsDataObject = productDataObject.getDataObject("reviews");
+        product.setReviews(createReviews(reviewsDataObject));
+        LOGGER.debug(product);
+        return product;
+    }
+
+    private Map<String, String> createReviews(DataObject reviewsDataObject) {
+        List<?> reviewsDataObjects = reviewsDataObject.getList("review");
+        Map<String, String> reviews = new LinkedHashMap<>();
+        for(Object reviewObject: reviewsDataObjects){
+            DataObject reviewDataObject = ((DataObject)reviewObject);
+            String author = reviewDataObject.getString("author");
+            String text = reviewDataObject.getString("text");
+            reviews.put(author,text);
+        }
+        LOGGER.debug(reviews);
+        return reviews;
+    }
+}
